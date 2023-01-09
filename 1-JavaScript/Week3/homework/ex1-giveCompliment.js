3bb187a35e0a8e398c5d377a894a6a5c0a2a45f9
Muhie Aldeen <zezo1998@hotmail.com>
2023-01-09 15:14:12 +0100

    I solved the first two exercises of the homewark w3

--- 1-JavaScript/Week3/homework/ex1-giveCompliment.js
+++ 1-JavaScript/Week3/homework/ex1-giveCompliment.js
@@ -18,17 +18,25 @@ Full description at: https://github.com/HackYourFuture/Homework/tree/main/1-Java
    Use `console.log` each time to display the return value of the
    `giveCompliment` function to the console.
 -----------------------------------------------------------------------------*/
-function giveCompliment(/* TODO parameter(s) go here */) {
+function giveCompliment(name) {
+  let compliments = ['awesome' , 'great', 'nice','inspiring' ,'breathtaking' , 'amazing' , 'astonishing' , 'smart' , 'intelligent' , ' brilliant'];
   // TODO complete this function
+  console.log( 'You are' + ' ' + compliments[Math.floor(Math.random() * compliments.length)] +' '+ name);
+  // return compliments[Math.floor(Math.random() * compliments.length )];
+  // return ['You are' + compliments [Math.floor(Math.random() * compliments.length)] + name];
 }
+
+giveCompliment('muhie')
 
 function main() {
   // TODO substitute your own name for "HackYourFuture"
-  const myName = 'HackYourFuture';
+  const myName = 'HackYourFuture'; 
+  const myOwnName =  myName.replace('HackYourFuture','MuhieAldeen');
+  // console.log( myName.replace('HackYourFuture','MuhieAldeen'));
 
-  console.log(giveCompliment(myName));
-  console.log(giveCompliment(myName));
-  console.log(giveCompliment(myName));
+  console.log(giveCompliment(myOwnName));
+  console.log(giveCompliment(myOwnName));
+  console.log(giveCompliment(myOwnName));
 
   const yourName = 'Amsterdam';
 
--- 1-JavaScript/Week3/homework/ex2-dogYears.js
+++ 1-JavaScript/Week3/homework/ex2-dogYears.js
@@ -16,9 +16,13 @@ calculate it!
    ages.
 -----------------------------------------------------------------------------*/
 
-function calculateDogAge(/* TODO parameter(s) go here */) {
+function calculateDogAge(number) { 
+  const age = number * 7 ;
+  return  'Your doggie is ' + age +  'years old in dog years!';
+// console.log( 'Your doggie is ' + age +  'years old in dog years!');
   // TODO complete this function
 }
+calculateDogAge()
 
 function main() {
   console.log(calculateDogAge(1)); // -> "Your doggie is 7 years old in dog years!"
