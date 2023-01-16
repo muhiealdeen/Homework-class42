0ca0c348d66ac0abd2cd40ecfbcf4f3268f383f3
Muhie Aldeen <zezo1998@hotmail.com>
2023-01-16 12:11:11 +0100

    tried to solve the 2nd and 3ed exe not fnished yet

--- 1-JavaScript/Week4/homework/ex3-lemonAllergy.test.js
+++ 1-JavaScript/Week4/homework/ex3-lemonAllergy.test.js
@@ -26,26 +26,25 @@ const fruitBasket = [
 ];
 
 // ! Function under test
-function sanitizeFruitBasket(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+function sanitizeFruitBasket(originalFruitBasketContents, toBeTakenOut) {
+  fruitBasket.filter((fruit) => fruit !== 'lemon');
 }
+const result = sanitizeFruitBasket(fruitBasket, 'lemon');
 
 // ! Unit tests (using Jest)
 describe('sanitizeFruitBasket', () => {
   test('should take two parameters', () => {
-    // TODO replace next line with your code
-    expect(false).toBe(true);
+    expect(sanitizeFruitBasket).toHaveLength(2);
   });
 
   test('should not modify the original `fruitBasket` array', () => {
     // Save the original contents of the fruit basket
     const originalFruitBasketContents = [...fruitBasket];
-    // TODO replace next line with your code
-    expect(false).toBe(true);
+    expect(fruitBasket).toEqual(result);
   });
 
   test('should return a new array that does not include the unwanted `lemon`', () => {
-    // TODO replace next line with your code
-    expect(false).toBe(true);
+    const expected = ['apple', 'grapefruit', 'banana', 'watermelon'];
+    expect(result).toEqual(expected);
   });
 });
