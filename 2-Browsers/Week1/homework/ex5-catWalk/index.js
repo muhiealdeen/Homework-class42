b073bc1e3932c52befcd5aaaee9f1b5b10903cf9
Muhie Aldeen <zezo1998@hotmail.com>
2023-04-18 23:21:33 +0200

    made the cat dance when it arrive to the middle for 5 second then go back to walk

--- 2-Browsers/Week1/homework/ex5-catWalk/index.js
+++ 2-Browsers/Week1/homework/ex5-catWalk/index.js
@@ -30,6 +30,23 @@ function catWalk() {
   if (moveToRight > window.innerWidth) {
     cat.style.left = '0px';
   }
+
+  const middleWidth = window.innerWidth / 2;
+  if (moveToRight > middleWidth) {
+    cat.src =
+      ' https:media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
+    cat.alt = 'Dancing Cat';
+  }
+
+  if (moveToRight > window.innerWidth / 2) {
+    cat.src =
+      ' https:media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
+    cat.alt = 'Dancing Cat';
+  }
+  setTimeout(function () {
+    cat.src = 'http://www.anniemation.com/clip_art/images/cat-walk.gif';
+    cat.alt = 'Cat walking';
+  }, 5000);
 }
 
 // TODO execute `catWalk` when the browser has completed loading the page
